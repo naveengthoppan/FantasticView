3453d9c1959f46ff6a92e923197221009df715f2
Naveen George Thoppan <naveenjbl@gmail.com>
2017-01-04 14:27:52 +0530

    Initial Commit

---
+++ FantasticView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FantasticView
+//
+//  Created by Naveen George Thoppan on 04/01/17.
+//  Copyright © 2017 Naveen George Thoppan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
